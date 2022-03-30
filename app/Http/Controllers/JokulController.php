7f8308cc3282e65b6a90d62ea99358bee14e68b8
sachrul.adzom <sachrul.adzom@gmail.com>
2022-03-30 16:58:49 +0700

    fixing

--- app/Http/Controllers/JokulController.php
+++ app/Http/Controllers/JokulController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 
 class JokulController extends Controller
@@ -11,8 +12,8 @@ class JokulController extends Controller
     private $paymentJokulEndpoint="https://api-sandbox.doku.com/checkout/v1/payment";
     private $clientId = "MCH-1040-1640168489748";
     private $secretKey="SK-XNHofeU4oqUhSO98812o";
-    private $baseWebHook="http://serverkita.com/webhook/jokul-payment";
-    private $jokulEndpoint="/doku-virtual-account/v2/payment-code";
+    private $baseWebHook="http://serverpunyakitayangbuatupdatestatus.com/webhook/jokul-payment";
+    private $jokulEndpoint="/checkout/v1/payment";
     private $requestId;
     private $requestDate;
     
@@ -29,38 +30,52 @@ class JokulController extends Controller
         $targetPath=$this->baseWebHook."/".$requestId;
         $this->targetPath = $targetPath;
 
-        $requestBody = [
-            "order"=> [
-                "amount"=>  90000,
-                "invoice_number"=> "INV-20210231-0001",
-                "line_items"=> [
-                    [
-                        "name"=> "T-Shirt Red",
-                        "price"=> 30000,
-                        "quantity"=> 2,
-                        "sku"=> "1101",
-                        "category"=> "Shirt"
-                    ]
-                ],
+        $requestBody = array(
+            "order"=> array(
+                "amount"=>  10500,
+                "invoice_number"=> "INV-648623403",
                 "currency"=> "IDR",
-                "callback_url"=> $this->baseWebHook,
-                
-            ],
-            "payment"=> [
+                "callback_url"=> "http://serverkita.com/webhook/jokul-payment",
+                "line_items"=> array(
+                    array(
+                        "name"=> "DOKU T-Shirt",
+                        "price"=> 10500,
+                        "quantity"=> 1,
+                    )
+                )
+            ),
+            "payment"=> array(
                 "payment_due_date"=> 60,
-            ],
-            "customer"=> [
-                "id"=> "CUST-0001",
+            ),
+            "customer"=> array(
                 "name"=> "Anton Budiman",
-                "email"=> "anton@example.com",
-                "phone"=> "6285694566147",
-                "address"=> "Menara Mulia Lantai 8",
+                "email"=> "anton@doku.com",
+                "phone"=> "+6285694566147",
+                "address"=> "Plaza Asia Office Park Unit 3",
                 "country"=> "ID"
-            ]
-            ];
+            )
+        );
 
         $token = $this->getToken($requestBody);
-        echo $token;
+        $obj = json_decode($token);
+        if(isset($obj->error)){
+            echo $token;
+        } else if(isset($obj->response)){
+            $responObj=$obj->response;
+            if(!isset($responObj->payment)){
+                return "No Payment key";
+            }
+
+            $paymentObj=$responObj->payment;
+            if(!isset($paymentObj->url)){
+                return "No url key";
+            }
+
+            //echo $paymentObj->url;
+            return view('embed-jokul',['jokulUrl'=>$paymentObj->url]);
+        }
+
+        
     }
 
     public function getToken($requestBody){
@@ -70,26 +85,38 @@ class JokulController extends Controller
         $componentHeader = array("Client-Id"=>$this->clientId,
                                 "Request-Id"=>$this->requestId,
                                 "Request-Timestamp"=>$this->requestDate,
-                                "Signature"=>" HMACSHA256=" . $signature
+                                "Signature"=>$signature,
+                               // "Content-Type"=>"application/json",
+                                "Accept"=>"*/*"
         );
 
-        //print_r($componentHeader);
-        $response = Http::withHeaders($componentHeader)->post($this->paymentJokulEndpoint, $requestBody);
+        //Log::info(print_r($componentHeader,true));
+        $dataJson = json_encode($requestBody,JSON_PRETTY_PRINT);
+        $response = Http::withHeaders($componentHeader)
+                    ->withBody($dataJson,"application/json")->post($this->paymentJokulEndpoint);
 
+        //$obj = json_decode($response);
         return $response;
     }
 
     public function getSignature($requestBody){
-        $digestValue = base64_encode(hash('sha256', json_encode($requestBody), true));
+        Log::info(json_encode($requestBody,JSON_PRETTY_PRINT));
+        $digestSha=hash('sha256', json_encode($requestBody,JSON_PRETTY_PRINT));
+        $digestValue = base64_encode(hash('sha256', json_encode($requestBody,JSON_PRETTY_PRINT), true));
         $componentSignature = "Client-Id:{$this->clientId}\n".
                                 "Request-Id:{$this->requestId}\n".
                                 "Request-Timestamp:{$this->requestDate}\n".
                                 "Request-Target:{$this->jokulEndpoint}\n".
                                 "Digest:{$digestValue}";
 
-        $signature = base64_encode(hash_hmac('sha256', $componentSignature, $this->secretKey, true));
-        echo $componentSignature;
-        return $signature;
+        $hmac=hash_hmac('sha256', $componentSignature, $this->secretKey, true);
+        $signature = base64_encode($hmac);
+        // Log::info("Digest sha256: ".$digestSha);
+        // Log::info("Digest: ".$digestValue);
+        // Log::info($componentSignature);
+        // Log::info("Signature HMACSHA256: ".$hmac);
+
+        return "HMACSHA256=".$signature;
 
     }
 
--- resources/views/embed-jokul.blade.php
+++ resources/views/embed-jokul.blade.php
@@ -1,6 +1,17 @@
-<!DOCTYPE html>
 <html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://sandbox.doku.com/jokul-checkout-js/v1/jokul-checkout-1.0.0.js"></script>
+    </head>
     <body>
-        halo
+        <button id="checkout-button">Checkout Now</button>
+
+        <script type="text/javascript">
+        var checkoutButton = document.getElementById('checkout-button');
+        // Example: the payment page will show when the button is clicked
+        checkoutButton.addEventListener('click', function () {
+            loadJokulCheckout('{{$jokulUrl}}'); // Replace it with the response.payment.url you retrieved from the response
+        });
+        </script>
     </body>
 </html>
